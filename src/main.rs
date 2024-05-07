e2e593cb2de143cf8f321cb0281649152dcbdd12
Hladikes <adi.hladik@gmail.com>
2024-05-07 21:50:36 +0000

    major restructuring of transit_index

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 mod util;
+mod transit_index;
 
 use std::sync::Arc;
 use actix_cors::Cors;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use gtfs_structures::Gtfs;
 use serde::{Deserialize, Serialize};
+use transit_index::{StopPlatforms, TransitIndex};
+
+struct AppData<'a> {
+    transit_index: Arc<transit_index::TransitIndex<'a>>,
+}
 
 #[get("/stops")]
-async fn get_stops(data: web::Data<Arc<Gtfs>>) -> impl Responder {
+async fn get_stops(data: web::Data<AppData<'_>>) -> impl Responder {
     let (stops, time_taken) = util::measure(|| {
-        let stops: Vec<Arc<gtfs_structures::Stop>> = data.stops.values().cloned().collect();
+        let stops: Vec<Arc<gtfs_structures::Stop>> = data.transit_index.gtfs.stops.values().cloned().collect();
         stops
     });
     
@@ -27,12 +33,17 @@ async fn get_stops(data: web::Data<Arc<Gtfs>>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let gtfs = Gtfs::new("gtfs.zip").expect("Could not open gtfs.zip file");
     // let url = "https://www.arcgis.com/sharing/rest/content/items/aba12fd2cbac4843bc7406151bc66106/data";
     // let gtfs = Gtfs::from_url(url).unwrap();
-    let app_data = web::Data::new(Arc::new(gtfs));
 
+    let transit_index = TransitIndex::new();
+    transit_index.build_platforms();
+    transit_index.build_direct_trips();
+    transit_index.build_distances();
     
+    let app_data = web::Data::new(AppData {
+        transit_index: Arc::new(transit_index),
+    });
 
     HttpServer::new(move || {
         App::new()
